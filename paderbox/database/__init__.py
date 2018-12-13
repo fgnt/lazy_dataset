0a9248ee272295643c64f483050da0613c8cdc52
Christoph <cbj@mail.uni-paderborn.de>
2018-12-13 14:08:40 +0100

    add database.__init__.py

---
+++ paderbox/database/__init__.py
@@ -0,0 +1,15 @@
+from .database import (
+    DictDatabase,
+    JsonDatabase,
+    KaldiDatabase,
+)
+from . import (
+    ami,
+    audio_set,
+    chime,
+    chime5,
+    merl_mixtures,
+    reverb,
+    timit,
+    wsj_bss,
+)
